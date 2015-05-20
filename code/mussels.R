2f61949a45c467eb7fd431a3855bc95e66943d05
Simon Tietze <tietze@motorola.com>
2015-05-20 22:53:39 +0000

    added lda

--- code/mussels.R
+++ code/mussels.R
@@ -9,6 +9,11 @@ library(lubridate)
 library(rattle)
 library(SnowballC)
 library(tm)
+library(doMC)
+library(topicmodels)
+
+basepath = "/mnt//r-devel/simon//boston-yelp-challenge/data/"
+registerDoMC(cores = 7)
 
 # col name data required
 multi_model_matrix = function(df, emptycolname) {
@@ -27,11 +32,11 @@ multi_model_matrix = function(df, emptycolname) {
   spread(df, category, indicator, fill=F)
 }
 
-id2yelp = read.csv("~/Documents/yelp_boston/restaurant_ids_to_yelp_ids.csv")
+id2yelp = read.csv(paste0(basepath, "restaurant_ids_to_yelp_ids.csv"))
 
 
 
-train_lab = read.csv("~/Documents/yelp_boston/train_labels.csv")
+train_lab = read.csv(paste0(basepath, "train_labels.csv"))
 names(train_lab) = c("id", "date", "restaurant_id", "V1", "V2", "V3")
 
 train_yelp = gather(id2yelp, key, business_id, -restaurant_id) %>%
@@ -41,7 +46,7 @@ train_yelp = gather(id2yelp, key, business_id, -restaurant_id) %>%
 
 
 business = paste("[",
-                 paste(readLines("~/Documents/yelp_boston/yelp_academic_dataset_business.json"),
+                 paste(readLines(paste0(basepath, "yelp_academic_dataset_business.json")),
                        collapse=", "),
                  "]")
 business = flatten(fromJSON(business))
@@ -85,24 +90,63 @@ business_train$late_night=!is.na(business_train$hours.Friday.close) & (
     business_train$hours.Friday.close < 5)
 
 review = paste("[",
-               paste(readLines("~/Documents/yelp_boston/yelp_academic_dataset_review.json"),
+               paste(readLines(paste0(basepath, "yelp_academic_dataset_review.json")),
                      collapse=", "),
                "]")
-review=fromJSON(review)
+review=flatten(fromJSON(review))
+review$text = gsub("not\\s+", " not_", review$text)
+review$text = gsub("n't\\s+", " not_", review$text)
 rc = Corpus(VectorSource(review$text))
-rc = tm_map(rc, removePunctuation)
-rc = tm_map(rc, stemDocument)
-rc = tm_map(rc, removeWords, c(stopwords("english"))) 
 ndoc = length(rc)
-review.mat = as.data.frame(
-  as.matrix(
-    DocumentTermMatrix(rc, control=list(bounds=list(global=(c(ndoc/1000, ndoc/5)))))
-  )
-)
+review.mat = as.matrix(DocumentTermMatrix(rc,
+                                          control=list(stemming = TRUE, stopwords = TRUE, minWordLength = 3,
+                                                       removeNumbers = TRUE, removePunctuation = TRUE,
+                                                       bounds=list(global=c(ndoc/1000, ndoc/2)),
+                                                       weighting = function(x) weightTfIdf(x, normalize = FALSE))
+                                          )
+                       )
+review.mat = round(review.mat * 100)
+rowTotals = apply(review.mat , 1, sum)
+review.mat = review.mat[rowTotals> 0, ]     
+words = apply(review.mat, 2, function(x) sum(x)/sum(x>0))
 
+review.lda = LDA(review.mat, k=10, control=list(verbose=T))
+save(review.lda, file=paste0(basepath, "review.lda"))
+review.lda = load(file=paste0(basepath, "review.lda"))
+review.topics = topics(review.lda, 1)
+review.topics.terms <- terms(review.lda, 50)
+review.topics.terms
+
+review.lda@beta[,which(review.lda@terms=="notfresh")]
+
+
+
+
+review_topics = inner_join(select(review, -text),
+                           data.frame(review_id = review[rownames(review.mat),]$review_id,
+                                      review.lda@gamma)) %>%
+  inner_join(unique(select(business_train, business_id)))
+
+relevant_review_words = function(last_date) {
+  r = filter(review_topics, date <= last_date) %>% mutate(date = last_date) %>%
+    group_by(date, business_id) %>%
+    summarise_each(funs(mean), starts_with("X"))
+  #print(paste(last_date, nrow(r)))
+  return(r)
+}
+
+review_topics_tall = gather(review_topics, topic, value, starts_with("X"))
+
+review_topics_tall %<>%
+  group_by(business_id, topic) %>%
+  arrange(date) %>%
+  mutate(value = cumsum(value), numreview = row_number(), value = value/numreview)
+  
+
+reviews_till_date = business_train %>% group_by(date) %>% do(relevant_review_words(.$date[1]))
 
 tip = paste("[",
-            paste(readLines("~/Documents/yelp_boston/yelp_academic_dataset_tip.json"),
+            paste(readLines(paste0(basepath, "yelp_academic_dataset_tip.json")),
                   collapse=", "),
             "]")
 tip=fromJSON(tip)
@@ -110,7 +154,7 @@ tip=fromJSON(tip)
 
 
 user = paste("[",
-             paste(readLines("~/Documents/yelp_boston/yelp_academic_dataset_user.json"),
+             paste(readLines(paste0(basepath, "yelp_academic_dataset_user.json")),
                    collapse=", "),
              "]")
 user=fromJSON(user)
@@ -118,12 +162,19 @@ user=fromJSON(user)
 
 
 checkin = paste("[",
-                paste(readLines("~/Documents/yelp_boston/yelp_academic_dataset_checkin.json"),
+                paste(readLines(paste0(basepath, "yelp_academic_dataset_checkin.json")),
                       collapse=", "),
                 "]")
 checkin=fromJSON(checkin)
 
 
+business_train_frac=sample_frac(business_train, 0.01)
+rf.caret =train(select(business_train_frac,-V1,-V2,-V3,-date,-id,-restaurant_id,-business_id,
+                       -full_address,-open,-name,-latitude,-longitude,
+                       -type, -state, -city,-contains("hours.")),
+                business_train_frac$V1,
+                method="rf",
+                trControl = trainControl(number = 10, verboseIter=T))
 
 rpart=train(select(business_train,-V1,-V2,-V3,-id,-restaurant_id,-business_id,
                        -full_address,-open,-name,-latitude,-longitude,
@@ -141,10 +192,4 @@ rf=randomForest(select(business_train,-V1,-V2,-V3,-date,-id,-restaurant_id,-busi
                        -type, -state, -city,-contains("hours.")),
                 business_train$V3)
 
-business_train_frac=sample_frac(business_train, 0.1)
-rf.caret =train(select(business_train_frac,-V1,-V2,-V3,-date,-id,-restaurant_id,-business_id,
-                           -full_address,-open,-name,-latitude,-longitude,
-                           -type, -state, -city,-contains("hours.")),
-                    business_train_frac$V1,
-                method="rf",
-                trControl = trainControl(number = 10, verboseIter=T))
+
